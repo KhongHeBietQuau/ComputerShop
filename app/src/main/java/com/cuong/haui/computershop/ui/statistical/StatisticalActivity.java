e9d828e7157fcf63afea72c1cba3dd46045aa654
Nguyen Manh Cuong <cuongnguyen0862816585@gmail.com>
2023-04-30 23:26:05 +0700

    ss
    ss

--- app/src/main/java/com/cuong/haui/computershop/ui/statistical/StatisticalActivity.java
+++ app/src/main/java/com/cuong/haui/computershop/ui/statistical/StatisticalActivity.java
@@ -18,6 +18,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
@@ -28,6 +29,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StatisticalActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
     Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +37,59 @@ public class StatisticalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_statistical);
         toolbar = findViewById(R.id.toobar);
         ActionToolBar();
+        //
+        DatabaseReference myRefAdminTime = database.getReference("Products");
+        // lay thang bao hanh san pham
+        myRefAdminTime.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                DefaultFirst1.mangSpConLai.clear();
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    SanPhamMoi sanPhamMoi = postSnapshot.getValue(SanPhamMoi.class);
+                    if(sanPhamMoi!= null){
+                        DefaultFirst1.mangSpConLai.add(sanPhamMoi);
+                        //Toast.makeText(view.getContext(), sanPhamMoi.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Getting Post failed, log a message
+
+                // ...
+            }
+        });
+        //
+        DatabaseReference myRefAdminSale = database.getReference("SaleOrders");
+        // lay thang bao hanh san pham
+        myRefAdminSale.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                DefaultFirst1.allSaleOrder.clear();
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+
+                    SaleOrder sanPhamMoi = postSnapshot.getValue(SaleOrder.class);
+                    if(sanPhamMoi!= null){
+                        DefaultFirst1.allSaleOrder.add(sanPhamMoi);
+                        //Toast.makeText(view.getContext(), sanPhamMoi.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }
+                Ve();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Getting Post failed, log a message
+
+                // ...
+            }
+        });
+        //
+
+    }
+
+    private void Ve(){
         for(int i=0;i<DefaultFirst1.mangSpConLai.size();i++){
             DefaultFirst1.saleOrderbyProduct.add(new SanPhamBanRa(DefaultFirst1.mangSpConLai.get(i).product_id,0));
         }
@@ -67,8 +122,6 @@ public class StatisticalActivity extends AppCompatActivity {
         LineGraphSeries<DataPoint> series2 = new LineGraphSeries<DataPoint>(values2);
         graphbanra.addSeries(series2);
     }
-
-
 
     private void ActionToolBar() {
         setSupportActionBar(toolbar);
